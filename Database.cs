82be37c40003f2f06323ba490bccf9e8fe6a03a0
Daniel Haacke <dhaacke@umich.edu>
2020-03-12 16:53:27 -0400

    Initial Bare-Bones Entity Framework

---
+++ Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1 {
+    class Database {
+        public List<Course> Courses { get; set; }
+        public List<Section> Sections { get; set; }
+        public List<Instructor> Instructors { get; set; }
+    }
+
+    public class Course {
+        public int Id { get; set; }
+        public string Department { get; set; }
+        public string Number { get; set; }
+        public int Credits { get; set; }
+        public List<Section> Sections { get; set; }
+    }
+
+    public class Section {
+        public int Id { get; set; }
+        public string Day { get; set; }
+        public Course course { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+
+    public class Instructor {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Office { get; set; }
+        public List<Section> Sections = new List<Section>();
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            label1.Text = "Hello world!";
+            Instructor eric = new Instructor {
+                Id = 1,
+                Name = "Eric",
+                Phone = "248-762-4206",
+                Office = "CIS 237"
+            };
+            string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=ClassFeb20;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+    }
+}
